b5b62dc1ab00095ae9e34609c8a6ea39b55a1c14
Ferdinand Cuisin <ferdinand.c@meta-api.io>
2025-03-04 14:22:08 +0100

    feat(chore): add some configs.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,29 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  darkMode: 'class',
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
-        inter: ['Inter', 'sans-serif'],
-        poppins: ['Poppins', 'sans-serif'],
-        'fira-code': ['Fira Code', 'monospace'],
+        inter: ["Inter", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+        "fira-code": ["Fira Code", "monospace"],
       },
       colors: {
-        primary: '#64ffda',
-        secondary: '#8892b0',
+        primary: "#fca311",
+        secondary: "#8892b0",
+        light: "#ced4da",
         navy: {
-          DEFAULT: '#0a192f',
-          light: '#112240',
-          lighter: '#233554',
-          dark: '#020c1b',
+          DEFAULT: "#0a192f",
+          light: "#112240",
+          lighter: "#233554",
+          dark: "#020c1b",
         },
         slate: {
-          light: '#a8b2d1',
-          lighter: '#ccd6f6',
+          light: "#a8b2d1",
+          lighter: "#ccd6f6",
         },
       },
     },
   },
   plugins: [],
-}
+};
